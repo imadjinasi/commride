09adfc1c393c36fe8e86a023940a5d9ebfe3050d
hcysq <humancapital.ysq@gmail.com>
2026-09-19 02:43:31 +0700

    rebase(separation): restore apps/mobile/lib/src/active_ride/realtime_client.dart

--- apps/mobile/lib/src/active_ride/realtime_client.dart
+++ apps/mobile/lib/src/active_ride/realtime_client.dart
@@ -1,3 +1,4 @@
+import 'convoy_separation.dart';
 import 'live_group_models.dart';
 import 'location_provider.dart';
 
@@ -23,16 +24,24 @@ class ActiveRideSnapshotReceived extends ActiveRideRealtimeEvent {
   const ActiveRideSnapshotReceived({
     required this.rideId,
     required this.presences,
+    this.separation,
   });
 
   final String rideId;
   final List<LiveRiderPresence> presences;
+  final LiveConvoySeparation? separation;
 }
 
 class ActiveRidePresenceUpdated extends ActiveRideRealtimeEvent {
   const ActiveRidePresenceUpdated({required this.presence});
 
   final LiveRiderPresence presence;
+}
+
+class ActiveRideSeparationUpdated extends ActiveRideRealtimeEvent {
+  const ActiveRideSeparationUpdated({required this.separation});
+
+  final LiveConvoySeparation separation;
 }
 
 class ActiveRideQuickActionRaised extends ActiveRideRealtimeEvent {
