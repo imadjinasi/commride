91e3e2c0c13f9d416f5a8afc4117cb337c62be8b
hcysq <humancapital.ysq@gmail.com>
2026-09-18 18:47:33 +0700

    feat(location): add Ride location provider contract

---
+++ apps/mobile/lib/src/active_ride/location_provider.dart
@@ -0,0 +1,38 @@
+enum RideLocationPermission {
+  unknown,
+  denied,
+  deniedPermanently,
+  granted,
+}
+
+enum RideMovementState {
+  moving,
+  stopped,
+  unknown,
+}
+
+class RideLocationSample {
+  const RideLocationSample({
+    required this.latitude,
+    required this.longitude,
+    required this.observedAt,
+    required this.movement,
+  });
+
+  final double latitude;
+  final double longitude;
+  final DateTime observedAt;
+  final RideMovementState movement;
+}
+
+abstract interface class RideLocationProvider {
+  Stream<RideLocationSample> get samples;
+
+  Future<RideLocationPermission> checkPermission();
+
+  Future<RideLocationPermission> requestPermission();
+
+  Future<void> start();
+
+  Future<void> stop();
+}
