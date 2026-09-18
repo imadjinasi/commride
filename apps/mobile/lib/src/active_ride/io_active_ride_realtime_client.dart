eabc96a4227d86b921b349a139d304929d63660b
hcysq <humancapital.ysq@gmail.com>
2026-09-18 19:05:56 +0700

    style(realtime): format auth-header construction

--- apps/mobile/lib/src/active_ride/io_active_ride_realtime_client.dart
+++ apps/mobile/lib/src/active_ride/io_active_ride_realtime_client.dart
@@ -121,7 +121,9 @@ class IoActiveRideRealtimeClient implements ActiveRideRealtimeClient {
     final String token = await _authGateway.idToken();
     final ActiveRideSocket socket = await _socketConnector.connect(
       _liveUri(rideId),
-      headers: <String, String>{'authorization': <String>['Bearer', token].join(' ')},
+      headers: <String, String>{
+        'authorization': <String>['Bearer', token].join(' '),
+      },
     );
 
     if (!_shouldReconnect || generation != _generation || rideId != _rideId) {
