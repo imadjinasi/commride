a5aef9a7438d56349f58303af3ce7ea37cb3f4d2
hcysq <humancapital.ysq@gmail.com>
2026-09-19 12:21:20 +0700

    feat(live): pass Active Ride runtime into Ride detail

--- apps/mobile/lib/src/screens/ride/ride_screen.dart
+++ apps/mobile/lib/src/screens/ride/ride_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../active_ride/active_ride_runtime.dart';
+
 import '../../api/checkpoint_api.dart';
 import '../../api/club_ride_api.dart';
 import '../../api/ride_briefing_api.dart';
@@ -18,6 +20,8 @@ class RideScreen extends StatefulWidget {
     required this.rideBriefingApi,
     required this.rideCommsApi,
     required this.rideSosApi,
+    this.activeRideRuntimeManager,
+    this.mapsEnabled = false,
     super.key,
   });
 
@@ -27,6 +31,8 @@ class RideScreen extends StatefulWidget {
   final RideBriefingApi rideBriefingApi;
   final RideCommsApi rideCommsApi;
   final RideSosApi rideSosApi;
+  final ActiveRideRuntimeManager? activeRideRuntimeManager;
+  final bool mapsEnabled;
 
   @override
   State<RideScreen> createState() => _RideScreenState();
@@ -146,6 +152,8 @@ class _RideScreenState extends State<RideScreen> {
           rideBriefingApi: widget.rideBriefingApi,
           rideCommsApi: widget.rideCommsApi,
           rideSosApi: widget.rideSosApi,
+          activeRideRuntimeManager: widget.activeRideRuntimeManager,
+          mapsEnabled: widget.mapsEnabled,
           onChanged: _refresh,
         ),
       ),
