732feea9f15c5b6c3acf21796bcc3c071c2a5e6a
hcysq <humancapital.ysq@gmail.com>
2026-09-19 13:23:42 +0700

    feat(recap): expose Recap API to Club Ride detail

--- apps/mobile/lib/src/screens/clubs/clubs_screen.dart
+++ apps/mobile/lib/src/screens/clubs/clubs_screen.dart
@@ -6,6 +6,7 @@ import '../../api/checkpoint_api.dart';
 import '../../api/club_ride_api.dart';
 import '../../api/ride_briefing_api.dart';
 import '../../api/ride_comms_api.dart';
+import '../../api/ride_recap_api.dart';
 import '../../api/ride_sos_api.dart';
 import '../../api/route_planner_api.dart';
 import '../../models/club_ride.dart';
@@ -20,6 +21,7 @@ class ClubsScreen extends StatefulWidget {
     required this.rideBriefingApi,
     required this.rideCommsApi,
     required this.rideSosApi,
+    this.rideRecapApi,
     this.activeRideRuntimeManager,
     this.mapsEnabled = false,
     super.key,
@@ -31,6 +33,7 @@ class ClubsScreen extends StatefulWidget {
   final RideBriefingApi rideBriefingApi;
   final RideCommsApi rideCommsApi;
   final RideSosApi rideSosApi;
+  final RideRecapApi? rideRecapApi;
   final ActiveRideRuntimeManager? activeRideRuntimeManager;
   final bool mapsEnabled;
 
@@ -163,6 +166,7 @@ class _ClubsScreenState extends State<ClubsScreen> {
           rideBriefingApi: widget.rideBriefingApi,
           rideCommsApi: widget.rideCommsApi,
           rideSosApi: widget.rideSosApi,
+          rideRecapApi: widget.rideRecapApi,
           activeRideRuntimeManager: widget.activeRideRuntimeManager,
           mapsEnabled: widget.mapsEnabled,
           onChanged: _refresh,
