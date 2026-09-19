f43eadb412cf58c77ec3fe3af6a552f704e279ad
hcysq <humancapital.ysq@gmail.com>
2026-09-19 13:22:46 +0700

    fix(live): remove redundant Geolocator imports

--- apps/mobile/lib/src/active_ride/geolocator_ride_location_provider.dart
+++ apps/mobile/lib/src/active_ride/geolocator_ride_location_provider.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:geolocator_android/geolocator_android.dart';
-import 'package:geolocator_apple/geolocator_apple.dart';
 
 import 'location_provider.dart';
 
